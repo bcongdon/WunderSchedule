f4bf679c649a987f1e7eacf9320e2cf606136a70
Benjamin Congdon <bencongdon96@gmail.com>
2016-05-03 11:33:52 -0500

    Fix to task.js; bugged variable

--- utils/task.js
+++ utils/task.js
@@ -64,7 +64,7 @@ exports.getTaskList = function(list_id, cb){
 exports.logTask = function(task_id, str, level){
     str = str || "Updated!";
     level = level || "info"
-    exports.getTask(curr.task_id, function(ret_task){
+    exports.getTask(task_id, function(ret_task){
         log.log(level, "'" + ret_task.title + "': " + str);
     });
 }
