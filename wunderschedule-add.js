c0a44b864964922bbccd42458024fa32088e4f80
Benjamin Congdon <bencongdon96@gmail.com>
2016-08-01 06:56:27 -0700

    Work towards 'add' command

---
+++ wunderschedule-add.js
@@ -0,0 +1,45 @@
+'use strict';
+
+require('datejs');
+var inquirer = require('inquirer');
+
+var prompts = [
+  {
+    name: 'name',
+    message: 'Task Name',
+    validate: function (input) {
+      return input.length > 0;
+    }
+  },
+  {
+    name: 'due_date',
+    message: 'Due Date',
+    validate: function (input) {
+      try {
+        var d = new Date.parse(input);
+        return d.getTime() > 0;
+      }
+      catch(e) {
+        return '\'' + input + '\' is not a date.'
+      }
+    }
+  },
+  {
+    name: 'start_time',
+    message: 'Task Start Time',
+    validate: function (input) {
+      return input.length > 0;
+    }
+  },
+  {
+    name: 'repeat_every',
+    message: 'Repeat Every',
+    validate: function (input) {
+      return input.length > 0;
+    }
+  }
+];
+
+inquirer.prompt(prompts).then(function(answers) {
+  console.log(answers)
+});
--- wunderschedule.js
+++ wunderschedule.js
@@ -6,7 +6,8 @@ var app = require('commander');
 
 app
     .version(pkg.version)
-    .command('auth', 'Authenticates WunderSchedule')
+    .command('auth',  'Authenticates WunderSchedule')
     .command('check', 'Runs a single check of the "scheduled" list')
     .command('watch', 'Watches your "scheduled" list and creates new tasks when necessary', {isDefault: true})
+    .command('add',   'Create a task template for WunderSchedule')
     .parse(process.argv);
